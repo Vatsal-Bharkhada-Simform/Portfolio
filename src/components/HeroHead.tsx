e771131d94a0d277e0cb40ead38a97a2cae2dbd7
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-01 16:16:46 +0530

    Refactor: Header item rendering logic

--- src/components/HeroHead.tsx
+++ src/components/HeroHead.tsx
@@ -1,9 +1,28 @@
+type NavItemType = {
+    href: string,
+    title: string
+}
+
+const navItems: NavItemType[] = [
+	{
+		href: "#projects",
+		title: "Projects",
+	},
+	{
+		href: "#about",
+		title: "About",
+	},
+	{
+		href: "#contact",
+		title: "Contact",
+	},
+];
+
 export default function HeroHead() {
 	function handleClick(e: React.MouseEvent, id: string) {
 		e.preventDefault();
 		const element = document.getElementById(id);
 		if (element && element instanceof HTMLElement) {
-			console.log("Scrolling");
 			element.scrollIntoView();
 		}
 	}
@@ -12,21 +31,13 @@ export default function HeroHead() {
 		<header className="fixed top-0 left-0 w-full p-2 flex justify-center">
 			<nav className="flex w-5xl px-4 py-4 md:px-12 md:py-8 rounded-[20rem] backdrop-blur-md bg-beige-translucent">
 				<ul className="flex w-full justify-around">
-					<NavItem
-						href={"#projects"}
-						title={"Projects"}
-						onClick={handleClick}
-					/>
-					<NavItem
-						href={"#about"}
-						title={"About"}
-						onClick={handleClick}
-					/>
-					<NavItem
-						href={"#contact"}
-						title={"Contact"}
-						onClick={handleClick}
-					/>
+					{
+                        navItems.map((item) => {
+                            return (
+                                <NavItem href={item.href} title={item.title} onClick={handleClick} />
+                            )
+                        })
+                    }
 				</ul>
 			</nav>
 		</header>
