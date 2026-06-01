bfa309a26e4ae29ccb19068783742c69dc1343d3
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-01 10:41:11 +0530

    Feat: Add projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import About from "./layout/About";
 import Hero from "./layout/Hero";
+import Projects from "./layout/Projects";
 
 function App() {
 	return (
@@ -7,6 +8,7 @@ function App() {
 			<main className="bg-beige text-primary">
 				<Hero />
 				<About />
+				<Projects />
 			</main>
 		</>
 	);
---
+++ src/layout/Projects.tsx
@@ -0,0 +1,34 @@
+import { projects } from "../utils/projects";
+import arrow_link from "../assets/icons/arrow_link.svg";
+
+export default function Projects() {
+	return (
+		<>
+			<section className="min-h-screen flex flex-col gap-12 bg-white px-12 md:px-48 py-24">
+				<div>
+					<h2 className="text-5xl">Projects</h2>
+				</div>
+				<div className="py-8 grid grid-cols-2 grid-flow-row gap-12">
+					{projects.map((project) => {
+						return (
+							<div key={project.id}>
+								<img
+									src={project.imageUrl}
+									alt={`${project.name} image`}
+                                    className="rounded-2xl"
+								/>
+								<div className="py-8 px-2 flex flex-col gap-4">
+									<h3 className="text-2xl font-semibold">
+										<a href={project.url} className="no-underline hover:underline">{project.name}</a>
+                                        <img src={arrow_link} alt="link icon" className="w-5 inline-block ml-2 align-middle" />
+									</h3>
+									<div className="font-inter font-light">{project.description}</div>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</section>
+		</>
+	);
+}
