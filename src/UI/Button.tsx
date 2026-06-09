95bc6f2babb6f1f8c1178967872f2096dc5996fd
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-09 15:50:42 +0530

    Fix: Destructure class name in button component to prevent style overriding

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -9,10 +9,10 @@ const buttonClassNames: Record<Variant, string> = {
 		"bg-[#312a22] text-white cursor-pointer px-6 py-2 md:py-4 rounded-4xl text-lg md:text-xl tracking-normal inset-shadow-button hover:inset-shadow-button-hover transition-all duration-300",
 };
 
-export default function Button({ variant, ...props }: ButtonProps) {
+export default function Button({ variant, className = "", ...props }: ButtonProps) {
 	return (
 		<button
-			className={`${buttonClassNames[variant]} ${props.className ?? ""}`}
+			className={`${buttonClassNames[variant]} ${className ?? ""}`}
 			{...props}
 		>
 			{props.children}
