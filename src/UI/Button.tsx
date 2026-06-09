32b275c9fa412e9e74c07fd9f34367d49a6ca604
Vatsal Bharkhada <vatsal.bharkhada@simformsolutions.com>
2026-06-09 09:56:58 +0530

    Update: Destructure important props in button and input components. Add props assignment in button component

--- src/UI/Button.tsx
+++ src/UI/Button.tsx
@@ -11,7 +11,10 @@ const buttonClassNames: Record<Variant, string> = {
 
 export default function Button({ variant, ...props }: ButtonProps) {
 	return (
-		<button className={`${buttonClassNames[variant]} ${props.className ?? ""}`}>
+		<button
+			className={`${buttonClassNames[variant]} ${props.className ?? ""}`}
+			{...props}
+		>
 			{props.children}
 		</button>
 	);
--- src/UI/Input.tsx
+++ src/UI/Input.tsx
@@ -2,19 +2,24 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 	labelText?: string;
 }
 
-export default function Input({ labelText = "", ...props }: InputProps) {
+export default function Input({
+	labelText = "",
+	id,
+	type,
+	...props
+}: InputProps) {
 	if (labelText) {
 		return (
 			<div className="flex flex-col gap-1">
 				<label
-					htmlFor={props.id}
+					htmlFor={id}
 					className="text-base md:text-lg font-inter tracking-tight text-primary"
 				>
 					{labelText}
 				</label>
 				<input
-					type={props.type}
-					id={props.id}
+					type={type}
+					id={id}
 					className="w-full px-4 py-2 bg-white border-2 border-primary/0 focus:border-primary/55 outline-none rounded-xl font-inter tracking-tight"
 					{...props}
 				/>
@@ -24,7 +29,8 @@ export default function Input({ labelText = "", ...props }: InputProps) {
 
 	return (
 		<input
-			type={props.type}
+			id={id}
+			type={type}
 			className="w-full px-4 py-2 bg-white border-2 border-primary/0 focus:border-primary/55 outline-none rounded-xl font-inter tracking-tight"
 			{...props}
 		/>
